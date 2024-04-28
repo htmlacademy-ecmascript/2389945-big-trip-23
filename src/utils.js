8f674bb33e86400724125feff43f592294b5ac25
hondavod70 <hondavod70@gmail.com>
2024-04-28 17:01:41 +0700

    Поправил формат длительности эвента

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,12 @@
 import dayjs from 'dayjs';
 
-//const DATE_FORMAT = 'DD/MM/YY HH:mm';
-
 export const getRandomArrayElement = (items) =>
   items[Math.floor(Math.random() * items.length)];
 
 export const formatEventDate = (date, formatPattern) =>
   date ? dayjs(date).format(formatPattern) : '';
 
-export const durationEventTime = (dateStart, dateEnd) => {
+export const eventDurationTime = (dateStart, dateEnd) => {
   const diff = dayjs(dateEnd).diff(dateStart, 'minute');
   return `${Math.floor(diff / 60)}H ${Math.floor(diff % 60)}M`;
 };
