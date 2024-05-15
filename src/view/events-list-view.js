17e7c51050a050cc92be3ef3456806389e0bfb7f
hondavod70 <hondavod70@gmail.com>
2024-05-15 17:40:25 +0700

    Наработки по ДЗ 5.10. Большие перемены (часть 1)

--- src/view/events-list-view.js
+++ src/view/events-list-view.js
@@ -2,7 +2,7 @@ import AbstractView from '../framework/view/abstract-view.js';
 
 const createEventsListTemplate = () => '<ul class="trip-events__list"></ul>';
 
-export default class EventListView extends AbstractView {
+export default class EventsListView extends AbstractView {
   get template() {
     return createEventsListTemplate();
   }
