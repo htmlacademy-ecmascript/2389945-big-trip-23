9a6376c96d0af3682de17badcc231a5635f13b33
hondavod70 <hondavod70@gmail.com>
2024-05-20 23:06:03 +0700

    Форматирование

--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -47,8 +47,8 @@ const createEventOffersTemplate = (availableOffers, selectedOffers) => {
 
   if (offersTemplate) {
     offersTemplate = `
-      <section class="event__section  event__section--offers">
-			  <h3 class="event__section-title  event__section-title--offers">
+      <section class="event__section event__section--offers">
+			  <h3 class="event__section-title event__section-title--offers">
           Offers
         </h3>
 				<div class="event__available-offers">
@@ -118,19 +118,19 @@ const createEventTypesTemplate = (offers, eventType) => {
   let typesTemplate = '';
   offers.forEach((offer) => {
     typesTemplate += `
-    <div class="event__type-item">
-      <input
-      id="event-type-${offer.type}-1"
-      class="event__type-input visually-hidden"
-      type="radio"
-      name="event-type"
-      value="${offer.type}"
-      ${offer.type === eventType ? 'checked' : ''}
-      >
-      <label class="event__type-label event__type-label--${offer.type}"
-      for="event-type-${offer.type}-1">${capitalizeFirstLetter(offer.type)}
-      </label>
-    </div>`;
+      <div class="event__type-item">
+        <input
+        id="event-type-${offer.type}-1"
+        class="event__type-input visually-hidden"
+        type="radio"
+        name="event-type"
+        value="${offer.type}"
+        ${offer.type === eventType ? 'checked' : ''}
+        >
+        <label class="event__type-label event__type-label--${offer.type}"
+        for="event-type-${offer.type}-1">${capitalizeFirstLetter(offer.type)}
+        </label>
+      </div>`;
   });
 
   return typesTemplate;
@@ -163,11 +163,11 @@ const createEventEditTemplate = (event, allDestinations, allOffers) => {
 	    <form class="event event--edit" action="#" method="post">
 	      <header class="event__header">
 		    <div class="event__type-wrapper">
-			    <label class="event__type  event__type-btn" for="event-type-toggle-1">
+			    <label class="event__type event__type-btn" for="event-type-toggle-1">
 				    <span class="visually-hidden">Choose event type</span>
 				    <img class="event__type-icon" width="17" height="17" src="./img/icons/${type}.png" alt="Event type icon">
 			    </label>
-			    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+			    <input class="event__type-toggle visually-hidden" id="event-type-toggle-1" type="checkbox">
 			    <div class="event__type-list">
 				    <fieldset class="event__type-group">
 					    <legend class="visually-hidden">Event type</legend>
@@ -176,33 +176,33 @@ const createEventEditTemplate = (event, allDestinations, allOffers) => {
 			    </div>
 		    </div>
 
-		    <div class="event__field-group  event__field-group--destination">
-			    <label class="event__label  event__type-output" for="event-destination-1">
+		    <div class="event__field-group event__field-group--destination">
+			    <label class="event__label event__type-output" for="event-destination-1">
 				  ${type}
 			    </label>
-			    <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destinationPoint.name}" list="destination-list-1">
+			    <input class="event__input event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destinationPoint.name}" list="destination-list-1">
 			    <datalist id="destination-list-1">
 				  ${destinationsTemplate}
 			    </datalist>
 		    </div>
 
-		    <div class="event__field-group  event__field-group--time">
+		    <div class="event__field-group event__field-group--time">
 			    <label class="visually-hidden" for="event-start-time-1">From</label>
-			    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${startDate}">
+			    <input class="event__input event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${startDate}">
 			    &mdash;
 			    <label class="visually-hidden" for="event-end-time-1">To</label>
-			    <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${endDate}">
+			    <input class="event__input event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${endDate}">
 		    </div>
 
-		    <div class="event__field-group  event__field-group--price">
+		    <div class="event__field-group event__field-group--price">
 			    <label class="event__label" for="event-price-1">
 				    <span class="visually-hidden">Price</span>
 				    &euro;
 			    </label>
-			    <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${totalPrice}">
+			    <input class="event__input event__input--price" id="event-price-1" type="text" name="event-price" value="${totalPrice}">
 		    </div>
 
-		    <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+		    <button class="event__save-btn btn btn--blue" type="submit">Save</button>
 		    <button class="event__reset-btn" type="reset">Delete</button>
 		    <button class="event__rollup-btn" type="button">
 			    <span class="visually-hidden">Open event</span>
